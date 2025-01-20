a155ce0cf15f8f5e5a6260f66a6c3061f5be3abc
abou-zithar <mahmouazbouzithar@gmail.com>
2025-01-20 18:35:41 +0200

    394. Decode String

--- 394. Decode String.py
+++ 394. Decode String.py
@@ -22,3 +22,31 @@ class Solution:
             print(stack)
     
         return current_string
+    
+
+
+
+class Solution2:
+
+    
+    def decodeString(self, s: str) -> str:
+        stack = []  
+        
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                current_string = ""  
+
+                while stack[-1] != "[":
+                    current_string =   stack.pop() + current_string
+                
+                stack.pop()
+                current_num = "" 
+                while stack and stack[-1].isdigit():
+                    current_num =   stack.pop() + current_num
+
+                stack.append( int(current_num) * current_string)
+
+        return "".join(stack)
+        
